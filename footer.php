cd0ac758789fa4b3e8956594abe7aa77cb1d6428
Eric <ericdub@gmail.com>
2015-03-30 09:03:39 -0500

    This is something

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The template for displaying the site footer.
  *
  * Contains footer content and the closing of the main and #page div elements.
  *
